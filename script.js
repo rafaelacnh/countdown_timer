04bb4ab2a8ebf27f766964bc99bf004783ef4050
rafaela <rafaela.cunha@calindra.com.br>
2021-05-20 01:54:40 -0300

    add calculos e setIterval

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date()
 
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000
+    
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const mins = Math.floor(totalSeconds / 60) % 60
+    const seconds = Math.floor(totalSeconds) % 60
+    
+
+
 }
 
 countdown()
+
+setInterval(countdown, 1000)
